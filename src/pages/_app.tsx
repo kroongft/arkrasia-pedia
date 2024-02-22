21f9adbee5e8b967ee8d134ca39e5f346b9f5995
kroongft <51918951+kroongft@users.noreply.github.com>
2024-02-22 21:14:58 +0900

    add: styled-components, styled-reset 패키지 추가, 초기 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
+import GlobalStyle from '@/styles/GlobalStyle'
+import theme from '@/styles/theme'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>아크라시아 대백과</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
