490249f51b84c48d27e6440be09920039f0b07a0
kroongft <51918951+kroongft@users.noreply.github.com>
2024-03-20 21:28:43 +0900

    add: 메인 로고 추가, favicon 변경

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+      <Image src="/logo.png" width={332} height={40} alt="" />
+    </div>
+  )
+}
+
+export default Main
