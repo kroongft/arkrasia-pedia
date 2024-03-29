b889bd418e6abbf662b7952677943ad349ab4225
kroongft <51918951+kroongft@users.noreply.github.com>
2024-03-29 22:33:20 +0900

    add: 메인 검색 input 추가

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,28 @@
+import { Input } from '@nextui-org/react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import { BiSearch } from 'react-icons/bi'
 
 function Main() {
+  const [search, setSearch] = useState('')
   return (
-    <div>
-      <Image src="/logo.png" width={332} height={40} alt="" />
-    </div>
+    <>
+      <div className="flex py-20 justify-center">
+        <Image src="/logo.png" width={332} height={40} alt="" />
+      </div>
+      <div className="px-5">
+        <Input
+          color="success"
+          placeholder="캐릭터 닉네임을 검색해보세요."
+          radius="full"
+          value={search}
+          endContent={<BiSearch size={24} />}
+          onChange={(e) => {
+            setSearch(e.target.value)
+          }}
+        />
+      </div>
+    </>
   )
 }
 
