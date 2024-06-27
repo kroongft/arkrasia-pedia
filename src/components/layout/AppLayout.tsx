b5a80b62c44000d46f49b569ca186b9e714f2b79
kroongft <51918951+kroongft@users.noreply.github.com>
2024-06-27 19:36:11 +0900

    style: layout 패딩 추가

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -6,7 +6,7 @@ interface AppLayoutProps {
 function AppLayout({ children }: AppLayoutProps) {
   return (
     <div
-      className="flex justify-center"
+      className="flex justify-center px-4"
       style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
     >
       <div className="max-w-[860px] w-full">{children}</div>
