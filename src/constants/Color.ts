ebdc78e424e0d5e33bd3bdaeb9ad0f5505b2f8ef
kroongft <51918951+kroongft@users.noreply.github.com>
2024-09-09 18:57:53 +0900

    chore: 장비 등급 색상 상수 설정 및 tailwind.config 설정

---
+++ src/constants/Color.ts
@@ -0,0 +1,9 @@
+export const GradeColor = {
+  고급: 'bg-gradient-to-tl from-고급 to-default',
+  희귀: 'bg-gradient-to-tl from-희귀 to-default',
+  영웅: 'bg-gradient-to-tl from-영웅 to-default',
+  전설: 'bg-gradient-to-tl from-전설 to-default',
+  유물: 'bg-gradient-to-tl from-유물 to-default',
+  고대: 'bg-gradient-to-tl from-고대 to-default',
+  에스더: 'bg-gradient-to-r from-에스더 to-default',
+} as { [key: string]: string }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,33 @@ const config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
+  safelist: [
+    {
+      pattern: /bg-gradient-to-tl/,
+    },
+    {
+      pattern: /from-(고급|희귀|영웅|전설|유물|고대|에스더)/,
+    },
+    {
+      pattern: /to-default/,
+    },
+  ],
   theme: {
-    extend: {},
+    gradientColorStops: (theme) => ({
+      ...theme('colors'),
+    }),
+    extend: {
+      colors: {
+        default: '#3d3325',
+        에스더: '#2faba8',
+        고대: '#dcc999',
+        유물: '#a24006',
+        전설: '#9e5f04',
+        영웅: '#480d5d',
+        희귀: '#113d5d',
+        고급: '#304911',
+      },
+    },
   },
   darkMode: 'class',
   plugins: [nextui()],
